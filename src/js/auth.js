a7287241a69ad7127a7bd0671a8143c36ad23475
King-George1 <82467459+King-George1@users.noreply.github.com>
2022-10-03 20:25:13 +0000

    Included the constant variables to used within the application

---
+++ src/js/auth.js
@@ -0,0 +1,4 @@
+export const BASEURL = "https://freddy.codesubmit.io";
+export const ACCESS_TOKEN_KEY = "accessToken";
+const COOKIE_MAX_AGE = 15 * 60 * 1000; //access token will be stored for 15 minutes in the cookie storage
+const REFRESH_TOKEN_KEY = "refreshToken";

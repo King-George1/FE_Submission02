b53f17185e780a6f0b47781647fc6f2b1faacce1
King-George1 <82467459+King-George1@users.noreply.github.com>
2022-10-03 21:01:52 +0000

    Implemented functionalities to handle logging, validating user inputs when the user is logging on the views

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
+import { loginUser, setCookieAccessToken } from "./js/auth";
 import "./css/login.css";
 import "./assets/Freddys_Logo.svg";
-console.log("This is login");
-console.log("Why are you not working godoodod")
+
+
+const loginForm = document.getElementById("loginForm");
+const userNameInput = document.getElementById("input-username");
+const passwordInput = document.getElementById("input-password");
+const errorMessage = document.getElementById("error-message");
+
+
+//Function to handle client-side validation error
+const handleErrorLogIn = (errorMsg) => {
+    errorMessage.innerHTML = errorMsg;
+    errorMessage.style.visibility = "visible"
+}
+
+
+
+//Function to handle logging in when the user clicks on the login button
+const handleLogin = (event) => {
+    event.preventDefault();
+    errorMessage.style.visibility = "hidden";
+    if (userNameInput.value === "" || passwordInput.value === "") {
+        handleErrorLogIn("Username or Password cannot be empty");
+        return;
+    }
+    try {
+        loginUser(userNameInput.value, passwordInput.value)
+            .then(res => {
+                if ("access_token" in res && "refresh_token" in res) {
+                    setCookieAccessToken(res.access_token, res.refresh_token);
+
+                    //Navigate to dashboard page if login is successful
+                    window.location.assign("./dashboard.html");
+                } else {
+                    if ("msg" in res) {
+                        handleErrorLogIn(res.msg);
+                    } else {
+                        handleErrorLogIn("Error Logging In");
+                    }
+                }
+            })
+            .catch(err => {
+                handleErrorLogIn("Error Logging In");
+            })
+    } catch (err) {
+        handleErrorLogIn("Error Logging In");
+    }
+}
+
+
+loginForm.addEventListener("submit", handleLogin);
